7136786e2bd54d531acaf17f05555ae06ad3ec09
varshitan <varshitha7819@gmail.com>
2023-08-09 18:26:07 +0530

    analyzed the data

--- flight_fare_predicton.py
+++ flight_fare_predicton.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 # Importing the training dataset
 train_data = pd.read_excel('Data_Train.xlsx')
 
@@ -38,32 +39,74 @@ print(train_data[train_data['Route'].isnull()])
 train_data.dropna(inplace=True)
 
 # Now the dataset has been cleaned by dropping rows with null values
-# creating a copy of this data so maupulations can be doneon the other dataset
 
-data =train_data.copy()
-# changing the datatype to datetime since machine cannot understand the object type 
-# instead of writing long codes we define a function because whenever there is a repititon of data its better to defne a function 
+# Creating a copy of this data so manipulations can be done on the other dataset
+data = train_data.copy()
+
+# Changing the datatype to datetime since the machine cannot understand the object type 
+# Instead of writing long codes, we define a function because whenever there is a repetition of data, it's better to define a function
 def change_into_datetime(col):
     data[col] = pd.to_datetime(data[col], dayfirst=True)
-# giving all the feaures that needs to convert in a list and then caling chage_into_datetime function
-for feature in ['Dep_Time', 'Arrival_Time' , 'Date_of_Journey']:
+
+# Giving all the features that need to be converted in a list and then calling the change_into_datetime function
+for feature in ['Dep_Time', 'Arrival_Time', 'Date_of_Journey']:
     change_into_datetime(feature)
-# after converting the data types we look into it again it make sure it has done 
+
+# After converting the data types, we look into it again to make sure it has been done
 print(data.dtypes)
-# converting the date of jounery column to day,month and year for better analysis
+
+# Converting the date of journey column to day, month, and year for better analysis
 data['Journey_day'] = data['Date_of_Journey'].dt.day
 data['Journey_month'] = data['Date_of_Journey'].dt.month
 data['Journey_year'] = data['Date_of_Journey'].dt.year
+
 # Reading the data
 print(data.head(2))
-# Now converting he time feature t  hours, mins
-def extract_hour_min(df , col):
-    df[col +'_hour'] = df[col].dt.hour
+
+# Now converting the time feature to hours and minutes
+def extract_hour_min(df, col):
+    df[col + '_hour'] = df[col].dt.hour
     df[col + '_minute'] = df[col].dt.minute
-    return df.head()
-print(extract_hour_min(data , 'Dep_Time'))
-print(extract_hour_min(data , 'Arrival_Time'))
-# After extrating these features we can drop the previous columnssince the all contains the sameinfo butin dfferent columns and types
-cols_to_drop = ['Arrival_Time' , 'Dep_Time']
-data.drop(cols_to_drop, axis =1 , inplace = True)
-data.head()
+    return df
+
+print(extract_hour_min(data, 'Dep_Time'))
+print(extract_hour_min(data, 'Arrival_Time'))
+
+# After extracting these features, we can drop the previous columns since they all contain the same info but in different columns and types
+cols_to_drop = ['Arrival_Time', 'Dep_Time']
+data.drop(cols_to_drop, axis=1, inplace=True)
+
+# Now we will start analyzing the data
+
+# Let's analyze in which time most of the flights take off
+def flight_dep_time(x):
+    if (x <= 4) and (x <= 8):
+        return 'Early morning'
+    elif (x > 8) and (x <= 12):
+        return 'Morning'
+    elif (x > 12) and (x <= 16):
+        return 'Noon'
+    elif (x > 16) and (x <= 20):
+        return 'Evening'
+    elif (x > 20) and (x <= 2):
+        return 'Night'
+    else:
+        return 'Late night'
+
+# To know the number of flights we use the value count method to it
+data['Dep_Time_Hour_category'] = data['Dep_Time_hour'].apply(flight_dep_time)
+flight_counts = data['Dep_Time_Hour_category'].value_counts()
+
+print(flight_counts)
+# plotting the flght counts for better understanding of data
+
+
+# To make graphs itnteractive we use plotly
+
+plt.bar(flight_counts.index, flight_counts, color='blue')
+plt.xlabel('Departure Time Category')
+plt.ylabel('Number of Flights')
+plt.title('Number of Flights by Departure Time Category')
+plt.xticks(rotation=45)
+plt.show()
+
