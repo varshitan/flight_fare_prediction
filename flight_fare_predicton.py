7cf4f5d67282bdc15de70f0986876c5cc0813301
varshitan <varshitha7819@gmail.com>
2023-08-09 16:08:48 +0530

    Reading dataset

---
+++ flight_fare_predicton.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mlt
+import seaborn as sns
+
+data = pd.read_excel('Data_Train.xlsx')
+print(data.head(10))
+print(data.describe())
+
+
+
+
